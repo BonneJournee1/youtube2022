6c293f400c2688998bcdce80b2be200515285fdb
BonneJournee1 <andreykaa.study.an@gmail.com>
2023-10-11 15:39:39 +0200

    add useFetch.js

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async() => {
+            setLoading(true);
+            try {
+                const res = axios.get(url);
+                setData(res.data);
+            } catch (err) { setError(err); }
+            setLoading(false);
+        };
+        fetchData();
+    }, [url]);
+}
